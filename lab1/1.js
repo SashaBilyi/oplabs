0afecaf6c0c7957eeb91b1e6b158f48687f3d163
SashaBilyi <biliy.sasha14032000@gmail.com>
2024-12-04 15:38:48 +0200

    Add files via upload

---
+++ lab1/1.js
@@ -0,0 +1,11 @@
+<<<<<<< HEAD:1.js
+let a = 17;
+const inc = (n) => ++n;
+console.dir(inc(a));
+
+=======
+let a = 16;
+const inc = (n) => ++n;
+console.dir(inc(a));
+
+>>>>>>> f964f151f94baf46ecc6deb4658487974decaff0:lab1/1.js
