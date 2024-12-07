dbc9f7ef7a5b30704ce4c468070488fee7045793
SashaBilyi <biliy.sasha14032000@gmail.com>
2024-12-07 10:57:10 +0200

    Delete lab7/3.js

--- lab7/3.js
+++
@@ -1,8 +0,0 @@
-function unique(array) {
-    return Array.from(new Set(array));
-  }
-  
-  const result = unique(['apple', 'banana', 'apple', 'cherry', 'banana']);
-  console.log(result);
-  // Результат: ['apple', 'banana', 'cherry']
-  
